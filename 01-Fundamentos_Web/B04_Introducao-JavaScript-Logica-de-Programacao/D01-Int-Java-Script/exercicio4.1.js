1106019b1003d3367611a6227b08a3d077e96079
jwmorais <j.wil.neto@gmail.com>
2021-11-26 19:38:24 -0300

    Conclusão do exercicio até o programa 5

---
+++ 01-Fundamentos_Web/B04_Introducao-JavaScript-Logica-de-Programacao/D01-Int-Java-Script/exercicio4.1.js
@@ -0,0 +1,25 @@
+const a;
+const b;
+const n1;
+const n2;
+const n3;
+
+let adicao = (a+b)
+let subtracao = (a-b)
+let multiplicacao = (a*b)
+let divisao = (a/b)
+let modulo = (a%b)
+
+if (n1>n2){
+    n1
+}else{
+    n2
+}
+
+if (n1>n2){
+    n1
+}else if (n2>n3{
+    n2
+}else{
+    n3
+}
