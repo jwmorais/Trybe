2d81ed8afed31ff94b73e1c5263977f1febc6321
jwmorais <j.wil.neto@gmail.com>
2021-11-29 22:30:54 -0300

    fechamento até as questões bonus

--- 01-Fundamentos_Web/B04_Introducao-JavaScript-Logica-de-Programacao/D02-Java-Array-LoopFor/exercicio4.2.js
+++ 01-Fundamentos_Web/B04_Introducao-JavaScript-Logica-de-Programacao/D02-Java-Array-LoopFor/exercicio4.2.js
@@ -1,4 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // 1-Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 for (let imprimir of numbers){
@@ -65,3 +66,41 @@ for (let i=0; i<num.length; i+=1){
     multiplicacao.push(num[i]/2)
 }
 console.log(multiplicacao)
+
+// Bonus
+// 1-Ordene o array numbers em ordem crescente e imprima seus valores;
+for (let index = 1; index < numbers2.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (numbers2[index] < numbers2[secondIndex]) {
+        let position = numbers2[index];
+        numbers2[index] = numbers2[secondIndex];
+        numbers2[secondIndex] = position;
+      }
+    }
+  }
+  console.log(numbers2)
+
+// 2-Ordene o array numbers em ordem decrescente e imprima seus valores;
+for (let index = 1; index < numbers2.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (numbers2[index] > numbers2[secondIndex]) {
+        let position = numbers2[index];
+        numbers2[index] = numbers2[secondIndex];
+        numbers2[secondIndex] = position;
+      }
+    }
+  }
+  console.log(numbers2)
+
+  // 3-Agora crie um novo array a partir do array numbers , sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push . O resultado deve ser o array abaixo: 
+// [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54]
+let numbersMultiplicado =[]
+for (let i=0; i<numbers.length; i+=1){
+    if (i<numbers.length-1){
+        numbersMultiplicado.push(numbers[i]*(numbers[1+i]))
+    }else{
+        numbersMultiplicado.push(numbers[i]*2)
+    }
+}
+console.log(numbers)
+console.log(numbersMultiplicado)
