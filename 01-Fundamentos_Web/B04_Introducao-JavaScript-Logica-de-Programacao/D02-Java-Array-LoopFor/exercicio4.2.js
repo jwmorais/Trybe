60dd8a6e62b3fa39557aff43be80c480dd643e1a
jwmorais <j.wil.neto@gmail.com>
2021-11-29 21:11:45 -0300

    fechamento até a questão 8

--- 01-Fundamentos_Web/B04_Introducao-JavaScript-Logica-de-Programacao/D02-Java-Array-LoopFor/exercicio4.2.js
+++ 01-Fundamentos_Web/B04_Introducao-JavaScript-Logica-de-Programacao/D02-Java-Array-LoopFor/exercicio4.2.js
@@ -34,8 +34,29 @@ console.log("O maior número do Array é: " + maiorNumero)
 // 6-Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 let impar=[];
 for (let i =0; i<numbers.length; i+=1){
-    if ((numbers[1])% 2 === 1){
+    if (numbers[i] % 2 == 1){
         impar.push(numbers[i])
     }
 }
-console.log(impar)
+if (impar.length>0){
+    console.log("Foram encontrados " + impar.length + " números ímpares.")
+}else{
+    console.log("nenhum valor ímpar encontrado")
+}
+
+// 7-Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+let menorNumero = numbers[0];
+for (let i = 1; i<numbers.length; i+=1)
+    if (numbers[i]<menorNumero){
+        menorNumero= numbers[i]
+    }
+console.log("O menor número do Array é: " + menorNumero)
+
+// 8-Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+let num = [];
+for (let i=1; i<=25; i+=1){
+    num.push(i)
+}
+console.log(num)
+
+// 9-Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
