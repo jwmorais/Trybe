2f42e0573b6cfc19c071a0ca9a17b6a8635c8375
jwmorais <j.wil.neto@gmail.com>
2021-11-29 21:28:31 -0300

    fechamento até a questão 9

--- 01-Fundamentos_Web/B04_Introducao-JavaScript-Logica-de-Programacao/D02-Java-Array-LoopFor/exercicio4.2.js
+++ 01-Fundamentos_Web/B04_Introducao-JavaScript-Logica-de-Programacao/D02-Java-Array-LoopFor/exercicio4.2.js
@@ -60,3 +60,8 @@ for (let i=1; i<=25; i+=1){
 console.log(num)
 
 // 9-Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+let multiplicacao = []
+for (let i=0; i<num.length; i+=1){
+    multiplicacao.push(num[i]/2)
+}
+console.log(multiplicacao)
